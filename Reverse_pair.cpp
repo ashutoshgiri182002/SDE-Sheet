40f6ea93298d30babc5666209c9483f91bb92d0b
Ashutosh Kumar Giri <93449692+ashutoshgiri182002@users.noreply.github.com>
2022-06-07 19:09:55 +0530

    https://leetcode.com/problems/reverse-pairs/

---
+++ Reverse_pair.cpp
@@ -0,0 +1,60 @@
+// TC --> O(NlogN) + o(N) + O(N)
+//SC --> O(N)
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int merge(vector<int>&nums,int low,int mid,int high){
+   int cnt = 0;
+   int j = mid+1;
+   for(int i = low;i<=mid;i++){
+     while(j<=high and nums[i] > 2 * nums[j]){
+       j++;
+     }
+     cnt += (j-(mid+1));
+   }
+
+   vector<int>temp;
+   int left = low, right = mid + 1;
+   while(left<=mid and right<=high){
+     if(nums[left]<=nums[right]){
+       temp.push_back(nums[left++]);
+     }
+     else{
+       temp.push_back(nums[right++]);
+     }
+   }
+   while(left <=mid){
+     temp.push_back(nums[left++]);
+   }
+   while(right <=high){
+     temp.push_back(nums[right++]);
+   }
+
+   for(int i = low; i<=high;i++){
+     nums[i] = temp[i-low];
+   }
+   return cnt;
+}
+
+int mergeSort(vector<int>&nums,int low, int high){
+  if(low>=high) return 0;
+  int mid = (low+high)/2;
+  int inv = mergeSort(nums,low,mid);
+  inv += mergeSort(nums,mid +1,high);
+  inv += merge(nums,low,mid,high);
+  return inv;
+}
+
+int reversePairs(vector<int>&nums){
+  return mergeSort(nums,0,nums.size()-1);
+}
+int main(){
+
+vector<int>v{40,25,19,12,9,6,2};
+
+cout<<reversePairs(v)<<endl;
+
+return 0 ;
+}
