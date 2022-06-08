95e7cf71f2b40c54fcf34731e2961ba4ed70f7fb
Ashutosh Kumar Giri <93449692+ashutoshgiri182002@users.noreply.github.com>
2022-06-08 17:29:58 +0530

    https://leetcode.com/problems/sort-colors/
    based on the Dutch national flag (DNF) algorithm

---
+++ sort_0_1_and_2.cpp
@@ -0,0 +1,44 @@
+/*
+Sort an array of 0's 1's and 2's
+
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void sortColors(vector<int>&nums){
+  int lo = 0;
+  int hi = nums.size()-1;
+  int mid = 0;
+
+  while(mid<=hi){
+    switch (nums[mid]) {
+      //if the element is 0
+      case 0:
+      swap(nums[lo++],nums[mid++]);
+      break;
+      //if the element is 1
+      case 1:
+      mid++;
+      break;
+      //if the element is 2
+      case 2:
+      swap(nums[hi--],nums[mid]);
+      break;
+    }
+  }
+}
+
+int main(){
+
+vector<int>v{0,1,0,1,1,2,1,0,1,0,2,0};
+
+sortColors(v);
+
+for(auto el:v){
+  cout<<el<<" ";
+}
+
+return 0;
+}
